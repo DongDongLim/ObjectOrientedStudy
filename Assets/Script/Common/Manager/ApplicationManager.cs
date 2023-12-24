776475240883c86181d38d0cb608aece9bb03c8a
Donghyen-Lim <99638155+DongDongLim@users.noreply.github.com>
2023-12-24 16:12:20 +0900

    feat : Update ApplicationManager

--- Assets/Script/Common/Manager/ApplicationManager.cs
+++ Assets/Script/Common/Manager/ApplicationManager.cs
@@ -7,17 +7,33 @@ namespace Study.Core
     public class ApplicationManager : SingletonMonoBehaviour<ApplicationManager>
     {
         private Subject<bool> _applicationPauseSubject;
+        private Subject<bool> _applicationFocusSubject;
+        private Subject<Unit> _applicationQuitSubject;
 
         public IObservable<bool> OnApplicationPauseObservable => _applicationPauseSubject;
+        public IObservable<bool> OnApplicationFocusObservable => _applicationFocusSubject;
+        public IObservable<Unit> OnApplicationQuitObservable => _applicationQuitSubject;
 
         protected override void Initialize()
         {
             _applicationPauseSubject = new Subject<bool>();
+            _applicationFocusSubject = new Subject<bool>();
+            _applicationQuitSubject = new Subject<Unit>();
         }
 
         private void OnApplicationPause(bool pause)
         {
             _applicationPauseSubject.OnNext(pause);
+        }
+
+        private void OnApplicationFocus(bool focus)
+        {
+            _applicationFocusSubject.OnNext(focus);
+        }
+
+        private void OnApplicationQuit()
+        {
+            _applicationQuitSubject.OnNext(Unit.Default);
         }
 
     }
